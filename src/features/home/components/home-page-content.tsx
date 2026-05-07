65cc0289179c1388bb98e5fe1bd9b58a96f60d1d
Bikash Santra <santrabikash921@gmail.com>
2026-05-07 19:31:06 +0530

    feat: home-enrollment-process section addedd

--- src/features/home/components/home-page-content.tsx
+++ src/features/home/components/home-page-content.tsx
@@ -1,13 +1,14 @@
 import { HomeHero } from "@/features/home/components/home-hero";
 import { HomeHeritageSection } from "@/features/home/components/home-heritage-section";
 import { HomePlatformFeaturesSection } from "@/features/home/components/home-platform-features-section";
+import { HomeEnrollmentProcessSection } from "./home-enrollment-process-section";
 
 export function HomePageContent() {
   return (
     <main className="bg-background">
       <HomeHero />
       <HomeHeritageSection />
-      {/* <HomeEnrollmentProcessSection /> */}
+      <HomeEnrollmentProcessSection />
       <HomePlatformFeaturesSection />
     </main>
   );
